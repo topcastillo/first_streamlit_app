f0c9a0f224d084b3f670d4f551748176114e34ee
topcastillo <124229855+topcastillo@users.noreply.github.com>
2023-02-08 20:53:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,8 @@ streamlit.dataframe(fruits_to_show)  #streamlit.dataframe(my_fruit_list)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + "kiwi")
+# [tn to delete]: streamlit.text(fruityvice_response.json())
 # take the json response and normalized it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output is a screen as a table
